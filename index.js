f30eb020a41a092a289f4478fcae2a479b415e3e
Manuel Vargas <manuelvargas@my.unt.edu>
2017-11-07 17:21:19 -0600

    x y adjust

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ $("#v1").hide();
 
 //expand card to fill container
 $("#card_button_1").click(function(){
-	let speed = 100;
+	let speed = "fast";
 	
 	$("#card_2").fadeOut(speed);
 	$("#card_3").fadeOut(speed);
 	
-	$("#v1").fadeIn(speed, function(){
+	$("#v1").fadeIn("slow", function(){
 		this.show();
 	});
 })
