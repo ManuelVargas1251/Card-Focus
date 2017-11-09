f0f32d4be39991ce2b6433ca2e5e77a5d94788fe
Manuel Vargas <manuelvargas@my.unt.edu>
2017-11-08 22:26:01 -0600

    more cards, sample video, and button toggle

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 //initially hide video player 1
 $("#v1").hide();
 
+$(".btn--open").text("Watch Now");
 
 //expand card to fill container
 $("#card_button_1").click(function(){
-	let speed = "fast";
 	
-	$("#card_2").fadeOut(speed);
-	$("#card_3").fadeOut(speed);
+	let speed = 0;
+
+	$("#card_2").toggle();
+	$("#card_3").toggle();
+	$("#v1").toggle();
+	$("#card_button_1")
+		.toggleClass("btn-primary")
+		.toggleClass("btn-danger")
+		.toggleClass("btn--open")
+		.toggleClass("btn--close");
 	
-	$("#v1").fadeIn("slow", function(){
-		this.show();
-	});
-})
+	$(".btn--open").text("Watch Now");
+	$(".btn--close").text("Close");
+});
